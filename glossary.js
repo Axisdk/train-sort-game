ef93131a333df82b0f07fd0aa866c63bf5caf17e
EryomenkoAlexander <sasha.eryomenko77@gmail.com>
2025-02-01 18:11:21 +0600

    delete: glossary file

--- glossary.js
+++
@@ -1,6 +0,0 @@
-export const glossary = [
-    { wagon: 'хуй', stone: 'за щеку' },
-    { wagon: 'много', stone: 'не пизди' },
-    { wagon: 'ибо', stone: 'нехуй' },
-    { wagon: 'елочка', stone: 'гори' },
-]
